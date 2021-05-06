5bcc875ea7030c3929925e478a72117d99b51b39
Farhad2590 <76680813+Farhad2590@users.noreply.github.com>
2021-05-06 23:46:40 +0600

    Add files via upload

---
+++ Program1/src/Pgrogram1/Program1.java
@@ -0,0 +1,17 @@
+package Pgrogram1;
+
+public class Program1 {
+	public static void main(String[] args) 
+	 { 
+	 System.out.println("\n Prog. is showing even no"); 
+	 for(int i=2;i<=20;i++) 
+	 { 
+	 if(i%2==0) 
+	 { 
+	 System.out.print("\n "+i); 
+	 } 
+	 
+	 } 
+	 } 
+
+}
